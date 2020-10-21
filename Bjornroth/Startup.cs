b0b7089a5f180b46d1ddd1acac698a27979e5629
Olivia Björner <filipokus.130@hotmail.com>
2020-10-21 13:44:37 +0200

    Controllers kopplade till Views

--- Bjornroth/Startup.cs
+++ Bjornroth/Startup.cs
@@ -35,7 +35,7 @@ namespace Bjornroth
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Start/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -50,7 +50,7 @@ namespace Bjornroth
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Start}/{action=Index}/{id?}");
             });
         }
     }
