34512e38794b01564d5e51640191ebbef42efcf6
Olivia Björner <filipokus.130@hotmail.com>
2020-10-26 18:43:56 +0100

    Javascript filen körs och likes uppdateras i labeln men inte i API:et - får ett felmeddelande om CORS-policy

--- Bjornroth/wwwroot/js/site.js
+++ Bjornroth/wwwroot/js/site.js
@@ -1,35 +1,62 @@
-﻿function ActivateEventListeners() {
-    for (let i = 0; i < 3; i++) {
-        document.getElementById(`like-btn${i}`).addEventListener("click", function (event) {
-            event.preventDefault()
-                <%#Bjornroth.CmdbRepository.UpdateRating(document.getElementById(`imdbId${i}`).ToString(), document.getElementById(`newRating${i}`).ToString) %>
-                UpdateRatingInView(i, "like")
-        })
-    }
+﻿alert('hej')
+document.getElementById(`like-btn0`).addEventListener("click", function (event) {
+    event.preventDefault()
+    userAction(document.getElementById(`imdbId${0}`).innerText)
+    UpdateRatingInView(0, "like")
 
-    for (let i = 0; i < 3; i++) {
-        document.getElementById(`dislike-btn${i}`).addEventListener("click", function (event) {
-            event.preventDefault()
-                <%#Bjornroth.CmdbRepository.UpdateRating(document.getElementById(`imdbId${i}`).ToString(), document.getElementById(`rating${i}`).ToString) %>
-                UpdateRatingInView(i, "dislike")
-        })
-    }
+
+})
+
+async function userAction (imdbId){
+    const response = await fetch(`https://localhost:44313/api/movie/${imdbId}`);
+    const myJson = await response.json();
 }
+//function ActivateEventListeners() {
+//    for (let i = 0; i < 3; i++) {
+//        document.getElementById(`like-btn${i}`).addEventListener("click", function (event) {
+//            event.preventDefault()
+//                <%#Bjornroth.CmdbRepository.UpdateRating(document.getElementById(`imdbId${i}`).ToString(), document.getElementById(`newRating${i}`).ToString) %>
+//                UpdateRatingInView(i, "like")
+//        })
+//    }
+
+//    for (let i = 0; i < 3; i++) {
+//        document.getElementById(`dislike-btn${i}`).addEventListener("click", function (event) {
+//            event.preventDefault()
+//                <%#Bjornroth.CmdbRepository.UpdateRating(document.getElementById(`imdbId${i}`).ToString(), document.getElementById(`rating${i}`).ToString) %>
+//                UpdateRatingInView(i, "dislike")
+//        })
+//    }
+//}
+
+
+//function UpdateRatingInView(index, typeOfRating) {
+//    if (typeOfRating === "dislike") {
+//        let rating = parseNumber(document.getElementById(`dislike-number${index}`).innerHTML)
+//        let newRating = rating + 1
+//        document.getElementById(`dislike-number${index}`).innerHTML = newRating
+//    }
+//    else {
+//        let rating = parseNumber(document.getElementById(`like-number${index}`).innerHTML)
+//        let newRating = rating + 1
+//        document.getElementById(`like-number${index}`).innerHTML = newRating
+//    }
+//}
+
+//window.onload = ActivateEventListeners();
+
+//alert(window.onload)
 
 
 function UpdateRatingInView(index, typeOfRating) {
     if (typeOfRating === "dislike") {
-        let rating = parseNumber(document.getElementById(`dislike-number${index}`).innerHTML)
-        let newRating = rating + 1
+        let rating = parseInt(document.getElementById(`dislike-number${index}`).innerHTML)
+        let newRating = rating+1
         document.getElementById(`dislike-number${index}`).innerHTML = newRating
     }
     else {
-        let rating = parseNumber(document.getElementById(`like-number${index}`).innerHTML)
-        let newRating = rating + 1
+        let rating = parseInt(document.getElementById(`like-number${index}`).innerHTML)
+        let newRating = rating+1
         document.getElementById(`like-number${index}`).innerHTML = newRating
     }
 }
-
-window.onload = ActivateEventListeners();
-
-alert(window.onload)
