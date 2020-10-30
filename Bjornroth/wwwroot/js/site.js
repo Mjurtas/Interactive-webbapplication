dedf18ff3c1ddc0f60995cb7cab0289297efc30f
Mjurtas <marten.bendroth@gmail.com>
2020-10-30 22:46:02 +0100

    Push design

--- Bjornroth/wwwroot/js/site.js
+++ Bjornroth/wwwroot/js/site.js
@@ -1,5 +1,5 @@
 ﻿async function testing(imdbId, newRating, index) {
-    const baseUrl = "https://localhost:44313/api/"
+    const baseUrl = "https://localhost:5001/api/"
     console.log(imdbId)
     try {
         const api = await fetch(`${baseUrl}movie/${imdbId}/${newRating}`, {
@@ -25,12 +25,7 @@
     }
 }
 
-function Rating() {
-    const likes = parseInt(document.getElementByClassName("number-of-likes-label"))
-    const dislikes = parseInt(document.getElementsByClassName("number-of-dislikes-label"))
-    const ratingPercentage = Math.round(((likes / (likes + dislikes) * 100)))
-    document.getElementsByClassName("rating-percentage-label").innerHTML = ratingPercentage + "%"
-}
+
 
 function helperSearch() {
     const search = document.getElementById("searchInput").value
@@ -62,21 +57,57 @@ function helperSearch() {
 
 let numberOfReactButtons = document.getElementsByClassName("submitBtn").length / 2
 
+
+function Rating() {
+    const likes = parseInt(document.getElementById("like-number0").innerHTML)
+    console.log(likes)
+    const dislikes = parseInt(document.getElementById("dislike-number0").innerHTML)
+    console.log(dislikes)
+    const ratingPercentage = Math.round(((likes / (likes + dislikes) * 100)))
+    console.log(ratingPercentage)
+    if (likes + dislikes > 0) {
+        document.getElementById("rating-percentage-label").innerHTML = ratingPercentage.toString() + "%"
+    }
+    else {
+        document.getElementById("rating-percentage-label").innerHTML = "N/A"
+    }
+
+    if (ratingPercentage > 50) {
+        document.getElementById("rating-percentage-label").style.color = "green"
+    }
+    else if (ratingPercentage < 50) {
+        document.getElementById("rating-percentage-label").style.color = "red"
+    }
+
+    else { document.getElementById("rating-percentage-label").style.color = "yellow" }
+}
+
+
+
 function activateEventListeners() {
     for (let i = 0; i < numberOfReactButtons; i++) {
         document.getElementById(`like-btn${i}`).addEventListener("click", function (event) {
             event.preventDefault()
+            Rating()
             testing(document.getElementById(`imdbId${i}`).value, "like", i)
         })
         document.getElementById(`dislike-btn${i}`).addEventListener("click", function (event) {
             event.preventDefault()
+            Rating()
             testing(document.getElementById(`imdbId${i}`).value, "dislike", i)
         })
     }
     document.getElementById("searchInput").addEventListener("input", helperSearch)
+}
 
+function ShortenPlot() {
+    const fullPlot = document.getElementById("full-plot").innerHTML
+    
+    const shortenedPlot = fullPlot.substr(0, 140) + "\u2026";
+    document.getElementById("shortened-plot").innerHTML = shortenedPlot
 }
 
 
 activateEventListeners()
-Rating()
+ShortenPlot()
+
