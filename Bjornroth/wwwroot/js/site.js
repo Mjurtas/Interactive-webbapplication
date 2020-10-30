7c00983e0256b2baa9dae72aa3faa40c2df4c00b
Olivia Björner <filipokus.130@hotmail.com>
2020-10-30 10:13:10 +0100

    WOHO API:et fungerar i frontend och uppdaterar likes i vyn

--- Bjornroth/wwwroot/js/site.js
+++ Bjornroth/wwwroot/js/site.js
@@ -1,16 +1,28 @@
-﻿async function testing(imdbId, newRating) {
+﻿async function testing(imdbId, newRating, index) {
     const baseUrl = "https://localhost:44313/api/"
     console.log(imdbId)
     try {
-        await fetch(`${baseUrl}movie/${imdbId}/${newRating}`, {
+        const api = await fetch(`${baseUrl}movie/${imdbId}/${newRating}`, {
             method: 'GET',
-            mode: 'no-cors'
+            mode: 'cors'
         })
+        if (api.ok) {
+            const readableApi = await api.json()
+            console.log(readableApi.numberOfLikes)
+            if (newRating === 'dislike') {
+                document.getElementById(`dislike-number${index}`).innerHTML = readableApi.numberOfDislikes
+            }
+            else {
+                document.getElementById(`like-number${index}`).innerHTML = readableApi.numberOfLikes
+            }
+        }
+        else {
+            throw api.status
+        }
     }
     catch (e) {
         console.log(e.message)
     }
-
 }
 
 function helperSearch() {
@@ -47,30 +59,15 @@ function activateEventListeners() {
     for (let i = 0; i < numberOfReactButtons; i++) {
         document.getElementById(`like-btn${i}`).addEventListener("click", function (event) {
             event.preventDefault()
-            testing(document.getElementById(`imdbId${i}`).value, "like")
-                UpdateRatingInView(i, "like")
+            testing(document.getElementById(`imdbId${i}`).value, "like", i)
         })
         document.getElementById(`dislike-btn${i}`).addEventListener("click", function (event) {
             event.preventDefault()
-            testing(document.getElementById(`imdbId${i}`).value, "dislike")
-            UpdateRatingInView(i, "dislike")
+            testing(document.getElementById(`imdbId${i}`).value, "dislike", i)
         })
     }
     document.getElementById("searchInput").addEventListener("input", helperSearch)
 
-}
-
-function UpdateRatingInView(index, typeOfRating) {
-    if (typeOfRating === "dislike") {
-        let rating = parseInt(document.getElementById(`dislike-number${index}`).innerHTML)
-        let newRating = rating+1
-        document.getElementById(`dislike-number${index}`).innerHTML = newRating
-    }
-    else {
-        let rating = parseInt(document.getElementById(`like-number${index}`).innerHTML)
-        let newRating = rating+1
-        document.getElementById(`like-number${index}`).innerHTML = newRating
-    }
 }
 
 
